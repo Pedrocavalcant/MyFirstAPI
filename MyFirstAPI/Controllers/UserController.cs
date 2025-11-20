e1d7c8a765f0143630407abb304b79e03626803b
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-11-20 11:05:54 -0300

    Feat, primeira rota da minha API

---
+++ MyFirstAPI/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    public IActionResult Get()
+    {
+        var response = new User
+        {
+            Age = 21,
+            Name = "Pedro",
+            Sobrenome = "Cavalcante",
+            Irmaos = 3,
+            Mae = "Maria Aparecida da Silva Cavalcante"
+        };
+        return Ok(response);
+    }
+}
---
+++ MyFirstAPI/User.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstAPI;
+
+public class User
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Irmaos { get; set; }
+    public string Sobrenome { get; set; }
+    public string Mae { get; set; }
+}

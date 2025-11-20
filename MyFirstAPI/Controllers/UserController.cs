08b46e6e9f06c3e81b5fc24009f672d38b00fc16
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-11-20 12:52:33 -0300

    Feat, adicionado mais endpoints

--- MyFirstAPI/Controllers/UserController.cs
+++ MyFirstAPI/Controllers/UserController.cs
@@ -12,7 +12,7 @@ public class UserController : ControllerBase
     [Route("{id}")]
     [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
-    public IActionResult Get([FromRoute] int id)
+    public IActionResult GetbyId([FromRoute] int id)
     {
         var response = new User
         {
@@ -45,6 +45,27 @@ public class UserController : ControllerBase
     [HttpDelete]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     public IActionResult Delete()
+    {
+        return NoContent();
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(List<User>), StatusCodes.Status200OK)]
+    public IActionResult GetAll()
+    {
+        var response = new List<User>()
+        {
+            new User { Id = 1, Name = "Pedro", Age = 21 },
+            new User { Id = 2, Name = "Lucas", Age = 21 },
+            new User { Id = 3, Name = "Mateus", Age = 18}
+        };
+
+        return Ok(response);
+    }
+
+    [HttpPut("change-password")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult ChangePassword([FromBody] RequestChangePasswordJson request)
     {
         return NoContent();
     }
